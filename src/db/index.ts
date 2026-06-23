226b7ed64c4b9fcf671a50d3fcb81099e3735f3b
impressivecoder1 <rohanrahman251@gmail.com>
2026-06-24 01:29:42 +0600

    database added

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,8 +6,28 @@ export const pool = new Pool({
 })
 
 export const initDB = async() => {
-    console.log('database connected');
     await pool.query(`
-        
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(200) NOT NULL,
+        email VARCHAR(250) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+        );
     `)
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+        type VARCHAR(20) NOT NULL,
+        status VARCHAR(20) DEFAULT 'open',
+        reporter_id INT NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+        );
+    `)
+    console.log('database connected');
 }
